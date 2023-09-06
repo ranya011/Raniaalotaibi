ec57d054688a52ede0d6265f7f90202a427efeab
Rania Alotaibi <rnnosh511@gmail.com>
2023-09-06 13:53:30 +0300

    Rania 251 Activity

---
+++ src/CPIT251.java
@@ -0,0 +1,17 @@
+/*
+Name: Rania Alotaibi
+ID: 2012278
+Group number: 4
+ */
+
+
+public class CPIT251 {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Rania Alotaibi 2012278");
+    
+    }
+    
+}
